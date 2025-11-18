c8c3ac29e7f678f532547d75a199e7bbb8cb005b
针不戳 <suxuejian19930220@gmail.com>
2025-11-18 17:00:25 +0800

    添加项目文件。

---
+++ MainForm.Designer.cs
@@ -0,0 +1,286 @@
+﻿
+namespace ModbusAddressScanner
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+        private void InitializeComponent()
+        {
+            gbSerialConfig = new GroupBox();
+            cboStopBits = new ComboBox();
+            label5 = new Label();
+            cboParity = new ComboBox();
+            label4 = new Label();
+            cboDataBits = new ComboBox();
+            label3 = new Label();
+            cboBaudRate = new ComboBox();
+            label2 = new Label();
+            cboPort = new ComboBox();
+            label1 = new Label();
+            gbControl = new GroupBox();
+            btnStop = new Button();
+            btnStart = new Button();
+            gbLog = new GroupBox();
+            txtLog = new TextBox();
+            label6 = new Label();
+            tb_readRegisterStart = new TextBox();
+            label7 = new Label();
+            tb_readRegisterCount = new TextBox();
+            gbSerialConfig.SuspendLayout();
+            gbControl.SuspendLayout();
+            gbLog.SuspendLayout();
+            SuspendLayout();
+            // 
+            // gbSerialConfig
+            // 
+            gbSerialConfig.Controls.Add(cboStopBits);
+            gbSerialConfig.Controls.Add(label5);
+            gbSerialConfig.Controls.Add(cboParity);
+            gbSerialConfig.Controls.Add(label4);
+            gbSerialConfig.Controls.Add(cboDataBits);
+            gbSerialConfig.Controls.Add(label3);
+            gbSerialConfig.Controls.Add(cboBaudRate);
+            gbSerialConfig.Controls.Add(label2);
+            gbSerialConfig.Controls.Add(cboPort);
+            gbSerialConfig.Controls.Add(label1);
+            gbSerialConfig.Location = new Point(12, 12);
+            gbSerialConfig.Name = "gbSerialConfig";
+            gbSerialConfig.Size = new Size(711, 100);
+            gbSerialConfig.TabIndex = 0;
+            gbSerialConfig.TabStop = false;
+            gbSerialConfig.Text = "串口配置";
+            // 
+            // cboStopBits
+            // 
+            cboStopBits.DropDownStyle = ComboBoxStyle.DropDownList;
+            cboStopBits.Location = new Point(477, 35);
+            cboStopBits.Name = "cboStopBits";
+            cboStopBits.Size = new Size(80, 25);
+            cboStopBits.TabIndex = 9;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(424, 38);
+            label5.Name = "label5";
+            label5.Size = new Size(47, 17);
+            label5.TabIndex = 8;
+            label5.Text = "停止位:";
+            // 
+            // cboParity
+            // 
+            cboParity.DropDownStyle = ComboBoxStyle.DropDownList;
+            cboParity.Location = new Point(338, 35);
+            cboParity.Name = "cboParity";
+            cboParity.Size = new Size(80, 25);
+            cboParity.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(285, 38);
+            label4.Name = "label4";
+            label4.Size = new Size(47, 17);
+            label4.TabIndex = 6;
+            label4.Text = "校验位:";
+            // 
+            // cboDataBits
+            // 
+            cboDataBits.DropDownStyle = ComboBoxStyle.DropDownList;
+            cboDataBits.Location = new Point(199, 35);
+            cboDataBits.Name = "cboDataBits";
+            cboDataBits.Size = new Size(80, 25);
+            cboDataBits.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(146, 38);
+            label3.Name = "label3";
+            label3.Size = new Size(47, 17);
+            label3.TabIndex = 4;
+            label3.Text = "数据位:";
+            // 
+            // cboBaudRate
+            // 
+            cboBaudRate.DropDownStyle = ComboBoxStyle.DropDownList;
+            cboBaudRate.Location = new Point(60, 66);
+            cboBaudRate.Name = "cboBaudRate";
+            cboBaudRate.Size = new Size(110, 25);
+            cboBaudRate.TabIndex = 3;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(7, 69);
+            label2.Name = "label2";
+            label2.Size = new Size(47, 17);
+            label2.TabIndex = 2;
+            label2.Text = "波特率:";
+            // 
+            // cboPort
+            // 
+            cboPort.DropDownStyle = ComboBoxStyle.DropDownList;
+            cboPort.Location = new Point(60, 35);
+            cboPort.Name = "cboPort";
+            cboPort.Size = new Size(80, 25);
+            cboPort.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(7, 38);
+            label1.Name = "label1";
+            label1.Size = new Size(47, 17);
+            label1.TabIndex = 0;
+            label1.Text = "端口号:";
+            // 
+            // gbControl
+            // 
+            gbControl.Controls.Add(tb_readRegisterCount);
+            gbControl.Controls.Add(label7);
+            gbControl.Controls.Add(tb_readRegisterStart);
+            gbControl.Controls.Add(label6);
+            gbControl.Controls.Add(btnStop);
+            gbControl.Controls.Add(btnStart);
+            gbControl.Location = new Point(12, 118);
+            gbControl.Name = "gbControl";
+            gbControl.Size = new Size(711, 70);
+            gbControl.TabIndex = 1;
+            gbControl.TabStop = false;
+            gbControl.Text = "扫描控制";
+            // 
+            // btnStop
+            // 
+            btnStop.Location = new Point(589, 25);
+            btnStop.Name = "btnStop";
+            btnStop.Size = new Size(100, 30);
+            btnStop.TabIndex = 1;
+            btnStop.Text = "停止扫描";
+            btnStop.UseVisualStyleBackColor = true;
+            btnStop.Click += btnStop_Click;
+            // 
+            // btnStart
+            // 
+            btnStart.Location = new Point(477, 25);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(100, 30);
+            btnStart.TabIndex = 0;
+            btnStart.Text = "开始扫描";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += btnStart_Click;
+            // 
+            // gbLog
+            // 
+            gbLog.Controls.Add(txtLog);
+            gbLog.Location = new Point(12, 194);
+            gbLog.Name = "gbLog";
+            gbLog.Size = new Size(711, 220);
+            gbLog.TabIndex = 2;
+            gbLog.TabStop = false;
+            gbLog.Text = "扫描日志";
+            // 
+            // txtLog
+            // 
+            txtLog.Dock = DockStyle.Fill;
+            txtLog.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            txtLog.Location = new Point(3, 19);
+            txtLog.Multiline = true;
+            txtLog.Name = "txtLog";
+            txtLog.ScrollBars = ScrollBars.Vertical;
+            txtLog.Size = new Size(705, 198);
+            txtLog.TabIndex = 0;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(7, 32);
+            label6.Name = "label6";
+            label6.Size = new Size(164, 17);
+            label6.TabIndex = 2;
+            label6.Text = "寄存器起始地址（十进制）：";
+            // 
+            // tb_readRegisterStart
+            // 
+            tb_readRegisterStart.Location = new Point(177, 29);
+            tb_readRegisterStart.Name = "tb_readRegisterStart";
+            tb_readRegisterStart.Size = new Size(100, 23);
+            tb_readRegisterStart.TabIndex = 3;
+            tb_readRegisterStart.Text = "1004";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(283, 32);
+            label7.Name = "label7";
+            label7.Size = new Size(104, 17);
+            label7.TabIndex = 4;
+            label7.Text = "读取寄存器长度：";
+            // 
+            // tb_readRegisterCount
+            // 
+            tb_readRegisterCount.Location = new Point(393, 29);
+            tb_readRegisterCount.Name = "tb_readRegisterCount";
+            tb_readRegisterCount.Size = new Size(66, 23);
+            tb_readRegisterCount.TabIndex = 5;
+            tb_readRegisterCount.Text = "1";
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(735, 426);
+            Controls.Add(gbLog);
+            Controls.Add(gbControl);
+            Controls.Add(gbSerialConfig);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "MainForm";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Modbus地址扫描工具";
+            FormClosing += MainForm_FormClosing;
+            gbSerialConfig.ResumeLayout(false);
+            gbSerialConfig.PerformLayout();
+            gbControl.ResumeLayout(false);
+            gbControl.PerformLayout();
+            gbLog.ResumeLayout(false);
+            gbLog.PerformLayout();
+            ResumeLayout(false);
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbSerialConfig;
+        private System.Windows.Forms.ComboBox cboStopBits;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cboParity;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cboDataBits;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cboBaudRate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox cboPort;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox gbControl;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.GroupBox gbLog;
+        private System.Windows.Forms.TextBox txtLog;
+        private Label label7;
+        private TextBox tb_readRegisterStart;
+        private Label label6;
+        private TextBox tb_readRegisterCount;
+    }
+}
---
+++ MainForm.cs
@@ -0,0 +1,334 @@
+﻿using Modbus.Device;
+using Modbus.Extensions.Enron;
+using System;
+using System.IO.Ports;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ModbusAddressScanner
+{
+    /// <summary>
+    /// 2025年11月18日，由豆包(https://www.doubao.com/)生成，只修改部分界面布局和添加依赖。
+    /// </summary>
+    public partial class MainForm : Form
+    {
+        // 串口相关变量
+        private System.IO.Ports.SerialPort _serialPort;
+        private ModbusSerialMaster _modbusMaster;
+        private CancellationTokenSource _cancellationTokenSource;
+        private bool _isScanning;
+        //发送读modbus寄存器指令中的起始地址和读寄存器个数
+        private int readRegistersStart;
+        private int readRegistersCount;
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeSerialPortOptions();
+            InitializeControls();
+        }
+
+        #region 初始化
+        /// <summary>
+        /// 初始化串口选项（端口号、波特率等）
+        /// </summary>
+        private void InitializeSerialPortOptions()
+        {
+            // 加载可用串口
+            cboPort.Items.AddRange(SerialPort.GetPortNames());
+            if (cboPort.Items.Count > 0)
+                cboPort.SelectedIndex = 0;
+
+            // 波特率选项
+            cboBaudRate.Items.AddRange(new object[] { 9600, 19200, 38400, 57600, 115200 });
+            cboBaudRate.SelectedItem = 9600;
+
+            // 数据位选项
+            cboDataBits.Items.AddRange(new object[] { 7, 8 });
+            cboDataBits.SelectedItem = 8;
+
+            // 校验位选项
+            cboParity.Items.AddRange(Enum.GetNames(typeof(Parity)));
+            cboParity.SelectedItem = Parity.None.ToString();
+
+            // 停止位选项
+            cboStopBits.Items.AddRange(Enum.GetNames(typeof(StopBits)));
+            cboStopBits.SelectedItem = StopBits.One.ToString();
+        }
+
+        /// <summary>
+        /// 初始化控件状态
+        /// </summary>
+        private void InitializeControls()
+        {
+            btnStop.Enabled = false;
+            txtLog.ReadOnly = true;
+            _isScanning = false;
+        }
+        #endregion
+
+        #region 串口操作
+        /// <summary>
+        /// 初始化串口和Modbus主机
+        /// </summary>
+        /// <returns>是否初始化成功</returns>
+        private bool InitSerialPort()
+        {
+            try
+            {
+                // 关闭已打开的串口
+                if (_serialPort != null && _serialPort.IsOpen)
+                    _serialPort.Close();
+
+                // 创建新串口实例
+                _serialPort = new SerialPort(
+                    cboPort.SelectedItem.ToString(),
+                    int.Parse(cboBaudRate.SelectedItem.ToString()),
+                    (Parity)Enum.Parse(typeof(Parity), cboParity.SelectedItem.ToString()),
+                    int.Parse(cboDataBits.SelectedItem.ToString()),
+                    (StopBits)Enum.Parse(typeof(StopBits), cboStopBits.SelectedItem.ToString())
+                );
+
+                // 串口参数配置
+                _serialPort.Handshake = Handshake.None;
+                _serialPort.ReadTimeout = 1000; // 读取超时1秒
+                _serialPort.WriteTimeout = 1000; // 写入超时1秒
+
+                // 打开串口
+                _serialPort.Open();
+
+                // 创建Modbus RTU主机
+                _modbusMaster = ModbusSerialMaster.CreateRtu(_serialPort);
+                _modbusMaster.Transport.ReadTimeout = 1000;
+                _modbusMaster.Transport.Retries = 0; // 不重试，提高扫描效率
+
+                Log($"串口初始化成功：{_serialPort.PortName}");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log($"串口初始化失败：{ex.Message}");
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 关闭串口
+        /// </summary>
+        private void CloseSerialPort()
+        {
+            try
+            {
+                if (_serialPort != null && _serialPort.IsOpen)
+                {
+                    _serialPort.Close();
+                    Log("串口已关闭");
+                }
+            }
+            catch (Exception ex)
+            {
+                Log($"关闭串口失败：{ex.Message}");
+            }
+        }
+        #endregion
+
+        #region Modbus扫描逻辑
+        /// <summary>
+        /// 开始地址扫描（异步执行，避免阻塞UI）
+        /// </summary>
+        private async void StartScan()
+        {
+            if (!InitSerialPort())
+                return;
+            if (!checkRegisterParam())
+            {
+                Log("寄存器参数有误，请检查");
+                return;
+            }
+            _isScanning = true;
+            _cancellationTokenSource = new CancellationTokenSource();
+            var token = _cancellationTokenSource.Token;
+
+            // 更新UI状态
+            UpdateControlStates(false);
+            Log("开始扫描Modbus地址（0-254）...");
+
+            try
+            {
+                // 地址从0到254递增扫描
+                for (byte address = 0; address <= 254; address++)
+                {
+                    if (token.IsCancellationRequested)
+                    {
+                        Log("扫描已取消");
+                        break;
+                    }
+
+                    Log($"正在扫描地址：{address}");
+
+                    // 发送Modbus指令（读取保持寄存器，示例：地址0，长度1）
+                    bool hasResponse = await SendModbusRequest(address, token);
+
+                    if (hasResponse)
+                    {
+                        // 收到返回，停止扫描
+                        Log($"✅ 地址 {address} 响应成功！扫描停止");
+                        MessageBox.Show($"找到有效Modbus地址：{address}", "扫描成功",
+                            MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        break;
+                    }
+
+                    // 扫描间隔（可调整，避免发送过快）
+                    await Task.Delay(100, token);
+                }
+
+                if (!token.IsCancellationRequested && _isScanning)
+                {
+                    Log("❌ 扫描完成，未找到有效地址");
+                    MessageBox.Show("所有地址扫描完毕，未收到任何响应", "扫描完成",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                Log($"扫描异常：{ex.Message}");
+            }
+            finally
+            {
+                // 清理资源
+                _isScanning = false;
+                CloseSerialPort();
+                UpdateControlStates(true);
+            }
+        }
+        
+        /// <summary>
+        /// 检查是否正确输入需要读取寄存器起始地址和读取寄存器数量
+        /// </summary>
+        /// <returns>输入参数是否正常</returns>
+        private bool checkRegisterParam()
+        {
+            try
+            {
+                readRegistersStart = int.Parse(tb_readRegisterStart.Text.Trim());
+                readRegistersCount = int.Parse(tb_readRegisterCount.Text.Trim());
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Log($"参数错误：{ex.Message}");
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 发送Modbus请求并检查响应
+        /// </summary>
+        /// <param name="slaveAddress">从站地址</param>
+        /// <param name="token">取消令牌</param>
+        /// <returns>是否收到响应</returns>
+        private async Task<bool> SendModbusRequest(byte slaveAddress, CancellationToken token)
+        {
+            try
+            {
+                // 示例：读取保持寄存器（功能码0x03），寄存器地址0，读取1个寄存器
+                // 可根据实际需求修改功能码和参数
+                var result = await Task.Run(() =>
+                {
+                    return _modbusMaster.ReadHoldingRegisters(
+                        slaveAddress, // 从站地址
+                        (ushort)readRegistersStart,            // 寄存器起始地址,1004-固件版本号
+                        (ushort)readRegistersCount             // 读取寄存器数量
+                    );
+                }, token);
+
+                // 有返回结果则表示响应成功
+                return result != null && result.Length > 0;
+            }
+            catch (Exception ex)
+            {
+                // Modbus无响应会抛出异常，直接返回false
+                Log($"地址 {slaveAddress} 无响应：{ex.Message.Split('\n')[0]}");
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// 停止扫描
+        /// </summary>
+        private void StopScan()
+        {
+            if (_cancellationTokenSource != null && !_cancellationTokenSource.IsCancellationRequested)
+            {
+                _cancellationTokenSource.Cancel();
+                _isScanning = false;
+                Log("正在停止扫描...");
+            }
+        }
+        #endregion
+
+        #region UI辅助方法
+        /// <summary>
+        /// 记录日志（线程安全）
+        /// </summary>
+        /// <param name="message">日志内容</param>
+        private void Log(string message)
+        {
+            if (txtLog.InvokeRequired)
+            {
+                txtLog.Invoke(new Action(() => Log(message)));
+                return;
+            }
+
+            txtLog.AppendText($"[{DateTime.Now:HH:mm:ss}] {message}{Environment.NewLine}");
+            txtLog.ScrollToCaret();
+        }
+
+        /// <summary>
+        /// 更新控件状态
+        /// </summary>
+        /// <param name="isIdle">是否处于空闲状态</param>
+        private void UpdateControlStates(bool isIdle)
+        {
+            if (InvokeRequired)
+            {
+                Invoke(new Action<bool>(UpdateControlStates), isIdle);
+                return;
+            }
+
+            // 扫描时禁用配置项和开始按钮，启用停止按钮
+            cboPort.Enabled = isIdle;
+            cboBaudRate.Enabled = isIdle;
+            cboDataBits.Enabled = isIdle;
+            cboParity.Enabled = isIdle;
+            cboStopBits.Enabled = isIdle;
+            btnStart.Enabled = isIdle;
+            btnStop.Enabled = !isIdle;
+        }
+        #endregion
+
+        #region 控件事件
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // 清空日志
+            txtLog.Clear();
+            // 开始扫描
+            StartScan();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            StopScan();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // 关闭窗口时停止扫描并关闭串口
+            if (_isScanning)
+                StopScan();
+
+            CloseSerialPort();
+        }
+        #endregion
+    }
+}
